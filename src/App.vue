2038dc28f00f2ea6928882b674cc1ab318bf553a
mouarius <mouarius@gmail.com>
2021-06-21 01:34:21 +0200

    First version with 1 card flipping

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,35 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+<archi-card>
+  Salut !
+</archi-card>
+
+
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import ArchiCard from './components/ArchiCard.vue'
+
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    ArchiCard
   }
 })
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,200;0,400;0,600;0,700;1,200;1,400&display=swap');
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color:#2b2b2b;
+  position: absolute;
+  top:0;
+  bottom:0;
+  left:0;
+  right:0;
+  display: flex;
+  justify-content: center;
+  font-family: 'Public Sans', sans-serif;
 }
 </style>
---
+++ src/components/ArchiCard.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="archicard" @click="revealAnswer">
+	  <div :class="{flipped: isRevealed}" class="archicard-inner">
+		<div class="card verso">
+			<h1>Une maison à Trentemoult</h1>
+			<h2>Architecte inconnu, 1990</h2>
+		</div>
+		<div class="card recto">
+			<img src="/src/assets/images/archi1.jpg" alt="" srcset="">	
+		</div>
+
+	  </div>
+  </div>
+</template>
+
+<script>
+export default {
+	name:"ArchiCard",
+	data(){
+		return({
+			isRevealed: false
+
+		}
+		)
+	},
+	methods:{
+		revealAnswer(){
+			this.isRevealed = !this.isRevealed
+			console.log("clicked !")
+			console.log(this.isRevealed)
+		}
+	}
+
+}
+</script>
+
+<style lang="scss">
+.archicard{
+	width: 320px;
+	height: 480px;
+	cursor: pointer;
+	perspective: 2000px;
+		
+}
+.archicard-inner{
+	transition: transform 0.6s ease;
+}
+.flipped{
+	transform: rotateY(180deg);
+}
+.card{
+	transform-style: preserve-3d;
+	display: block;
+	position:absolute;
+	width: 320px;
+	height: 480px;
+	box-sizing: border-box;
+	overflow: hidden;
+	margin-top: 24px;
+	border-radius: 24px;
+	border: solid 1px rgba(0, 0, 0, 0.1);
+	box-shadow: 0px 8px 20px rgba(0, 0, 0, 0.9);
+	color: rgb(247, 247, 247);
+	background-color: rgb(29, 29, 29);
+}
+.recto {
+	-webkit-backface-visibility: hidden; /* Safari */
+  	backface-visibility: hidden;
+	img{
+		width:auto;
+		height:100%;
+	}
+}
+.verso{
+	display: block;
+	padding: 16px;
+	transform: rotateY(180deg);
+	overflow: hidden;
+	text-overflow: clip;
+	white-space: nowrap;
+}
+</style>
